8c0dbb2b65c3e0e1ed8b89e36c1045bd10143c4a
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-11-09 22:59:30 +0100

    up

--- Kryptologia/Lab1/Lab1.py
+++ Kryptologia/Lab1/Lab1.py
@@ -3,19 +3,17 @@ import hashlib
 
 from oracle import encrypt, is_padding_ok, BLOCK_SIZE, _decrypt
 
-def attack( ciphertext ):
+def attack(ciphertext):
 	guessed_clear = b''
-
-	split_string = lambda x, n: [x[i:i+n] for i in range(0, len(x), n)]
-	blocks = split_string( ciphertext, BLOCK_SIZE )
+	blocks = get_blocks(ciphertext, BLOCK_SIZE)
 	
-	for block_n in range( len( blocks ) - 1, 0, -1 ): #build pair of blocks starting from end of message
+	for block_n in range( len(blocks) - 1, 0, -1 ): #build pair of blocks starting from end of message
 		spliced_ciphertext = blocks[block_n - 1] + blocks[block_n]
 
 		decoded_bytes = b'?' * BLOCK_SIZE #output of block cipher decoding values
 
 		##GET VALUE OF SECRET BYTE byte
-		for byte in range( BLOCK_SIZE - 1, -1, -1 ):
+		for byte in range(BLOCK_SIZE - 1, -1, -1):
 			new_pad_len = BLOCK_SIZE - byte
 
 			#Build hacked ciphertext tail with values to obtain desired padding
@@ -28,29 +26,33 @@ def attack( ciphertext ):
 				hacked_ciphertext = spliced_ciphertext[:byte] + attack_str + hacked_ciphertext_tail + spliced_ciphertext[byte + 1 + new_pad_len - 1:]
 
 				if( is_padding_ok( hacked_ciphertext ) ):
-
 					test_correctness = hacked_ciphertext[:byte - 1] + bytearray.fromhex( '{:02x}'.format( ( 1 ^  hacked_ciphertext[byte] ) ) )  + hacked_ciphertext[byte:]
 					if( not is_padding_ok( test_correctness ) ):
 						continue
 					
 					decoded_bytes = decoded_bytes[:byte] + bytearray.fromhex('{:02x}'.format( hacked_ciphertext[byte] ^ new_pad_len ) ) + decoded_bytes[byte + 1:]
 					guessed_clear = bytearray.fromhex('{:02x}'.format( i ^ new_pad_len ) ) + guessed_clear
-					break
-	
+					break	
 	return guessed_clear[:-guessed_clear[-1]] #remove padding!
 
-def test_the_attack():
-    messages = (b'Attack at dawn', b'', b'Giovanni',
-                b"In symmetric cryptography, the padding oracle attack can be applied to the CBC mode of operation," +
-                b"where the \"oracle\" (usually a server) leaks data about whether the padding of an encrypted " +
-                b"message is correct or not. Such data can allow attackers to decrypt (and sometimes encrypt) " +
-                b"messages through the oracle using the oracle's key, without knowing the encryption key")
+def get_blocks(ciphertext, siz):
+        out = [ciphertext[i:i+siz] for i in range(0, len(ciphertext), siz)]        
+        return out
+
+def run():
+    messages = (b'Attack at dawn',
+                b'',
+                b'Giovanni',
+                b"In symmetric cryptography, the padding oracle attack can be applied to the CBC mode of operation")
 
     for msg in messages:
-        print(f'{msg} OF LENGTH {len(msg)}')
-        cracked_ct = attack(encrypt(msg))
-        assert( cracked_ct == msg)
+        this_cipher = encrypt(msg)
+        cracked_msg = attack(this_cipher)
+        
+        # print(f'{msg}\n\t{cracked_msg}')
+
+        
 
 
 if __name__ == '__main__':
-    test_the_attack()
+        run()
--- Kryptologia/Lab1/oracle.py
+++ Kryptologia/Lab1/oracle.py
@@ -1,40 +1,42 @@
 from Crypto.Cipher import AES
 from Crypto import Random
-import sys
+# import sys
 
+
+#def __init__(self):
 KEY_LENGTH = 16  # AES128
 BLOCK_SIZE = AES.block_size
-
 _random_gen = Random.new()
 _key = _random_gen.read(KEY_LENGTH)
 
-
 def _add_padding(plain):
-	pad_len = BLOCK_SIZE - (len(plain) % BLOCK_SIZE)
-	padding = bytes([pad_len]) * pad_len
-	return plain + padding
+    pad_len = BLOCK_SIZE - (len(plain) % BLOCK_SIZE)
+    padding = bytes([pad_len]) * pad_len
+    return plain + padding
 
 def _remove_padding(data):
-	pad_len = data[-1]	
-	if pad_len < 1 or pad_len > BLOCK_SIZE:
-		return None
-	for i in range(1, pad_len):
-		if data[-i-1] != pad_len:
-			return None
-	return data[:-pad_len]
+        pad_len = data[-1]	
+        if pad_len < 1 or pad_len > BLOCK_SIZE:
+                return None #raise ValueError
+            
+        for i in range(1, pad_len):
+                if data[-i-1] != pad_len:
+                        return None
+                    
+        return data[:-pad_len]
 
 def encrypt(msg):
-	iv = _random_gen.read(AES.block_size)
-	cipher = AES.new(_key, AES.MODE_CBC, iv)
-	return iv + cipher.encrypt(_add_padding(msg))
+        iv = _random_gen.read(AES.block_size)
+        cipher = AES.new(_key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(_add_padding(msg))
 
 def _decrypt(data):
-	iv = data[:BLOCK_SIZE]
-	cipher = AES.new(_key, AES.MODE_CBC, iv)
-	return _remove_padding(cipher.decrypt(data[BLOCK_SIZE:]))
+        iv = data[:BLOCK_SIZE]
+        cipher = AES.new(_key, AES.MODE_CBC, iv)
+        return _remove_padding(cipher.decrypt(data[BLOCK_SIZE:]))
 
 def is_padding_ok(data):
-	return _decrypt(data) is not None
+        return _decrypt(data) is not None
 
 if __name__ == '__main__':
 	print("nothing...")
