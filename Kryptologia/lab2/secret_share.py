867fc2a64b224c9ca89018065c5e54f1abf7a159
Greg <grzesiekk321@gmail.com>
2019-11-11 16:41:51 +0100

    fixed missing module

--- Kryptologia/lab2/secret_share.py
+++ Kryptologia/lab2/secret_share.py
@@ -2,7 +2,7 @@ import numpy as np
 from binascii import hexlify
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from Crypto.Protocol.secret_sharing import Shamir
+from Crypto.Protocol.SecretSharing import Shamir
 
 
 key = get_random_bytes(16)
