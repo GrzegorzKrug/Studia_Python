f9a2fc7bca51bcc5d761022a1f6c6eb97017bce1
Greg <grzesiekk321@gmail.com>
2019-11-15 00:11:09 +0100

    Modular inverse, wikipedia

--- Kryptologia/lab2/secret_share.py
+++ Kryptologia/lab2/secret_share.py
@@ -5,7 +5,7 @@ from Crypto.Random import get_random_bytes
 # from Crypto.Protocol.SecretSharing import Shamir
 
 class SecretShare:
-	def __init__(self, secretInput=None, shareNum=3):
+	def __init__(self, secretInput=None, shareNum=3, quiet=True):
 		if not secretInput:
 				self.secret = [bt for bt in get_random_bytes(10)]
 		else:
@@ -16,9 +16,9 @@ class SecretShare:
 		#self.regions = [Region(3), Region(4), Region(10)]	
 		self.M = int.from_bytes(self.secret, byteorder='big')
 		self.S = None
+		self.quiet = quiet
 
-	def createShares(self, n):
-		
+	def createShares(self, n):		
 		p = 2*self.M + 1
 		p = 13 # FIX
 		if not (self.M > n and self.M < p):
@@ -30,29 +30,30 @@ class SecretShare:
 		# 	coeffs += [int.from_bytes(get_random_bytes(self.complexity),
 		# 				byteorder='big')]
 		poly = Polynomial(coeffs)
-		print(poly)		
+		if not self.quiet:
+			print(poly)		
 		self.S = []
+
 		for i in range(n):
 			s, txt = poly(i+1)
 			s = s % p
 			self.S += [s]
-			print(f"Share {i+1} = {s}")
+			# print(f"Share {i+1} = {s}")
+
 		#print(f"Coeffs: {coeffs}")
-
-	def inverse_modulo(a, n):
-		g = [n, a, 0]	
-		u = None
-
 
 	def run(self):		
 		A = ([bt ^ 125 for bt in self.secret])  # secret ^ 125
 		#out = ([bt ^ 125 for bt in A])
 		out = None
-		print(f"\nSekret:\n{self.concatenateBytes(self.secret)}")
-		print(f"Sekret liczbowo: {self.M}")
+		if not self.quiet:
+			print(f"\nSekret:\n{self.concatenateBytes(self.secret)}")
+			print(f"Sekret liczbowo: {self.M}")
 
 		self.createShares(self.shareNum)
-
+		if not self.quiet:
+			for i, s in enumerate(self.S):
+				print(f"Share {i+1} = {s}")
 
 		if out == self.secret:
 			print("Secret is the same")
@@ -63,7 +64,8 @@ class SecretShare:
 				print(f"{self.concatenateBytes(out)}")
 			except TypeError:
 				print(f"{out}")
-		
+		x = ModularInverse.mulinv(17, 43)  # TO DO
+		print(f"Inverse modulo 17^-1%43 = {x}")
 
 	def translateByteToText(self, textin):
 		return ''.join([chr(o) for o in textin])
@@ -94,6 +96,36 @@ class Polynomial:
 		return y, f'f({x}) = {y}'
 
 
+# Wikipedia
+# https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Modular_inverse
+class ModularInverse:	
+	@classmethod
+	def xgcd(cls, a, b):
+	    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+	    x0, x1, y0, y1 = 0, 1, 1, 0
+	    while a != 0:
+	        q, b, a = b // a, a, b % a
+	        y0, y1 = y1, y0 - q * y1
+	        x0, x1 = x1, x0 - q * x1
+	    return b, x0, y0
+
+	@classmethod
+	def egcd(cls, a, b):
+	    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+	    if a == 0:
+	        return (b, 0, 1)
+	    else:
+	        g, x, y = cls.egcd(b % a, a)
+	        return (g, y - (b // a) * x, x)
+
+	@classmethod
+	def mulinv(cls, a, b):
+	    """return x such that (x * a) % b == 1"""
+	    g, x, _ = cls.xgcd(a, b)
+	    if g == 1:
+        	return x % b
+
+
 class Region:
 	def __init__(self, senatorsNum=1):
 		self.senators = []
@@ -107,4 +139,5 @@ if __name__ == '__main__':
 	app = SecretShare(secretInput=get_random_bytes(30))
 	#app.run()
 
+	#a = ModularInverse.mulinv(17, 43)
 	input('End....')
