18921e79f6b610c64ea22f6b7acba8889734cad6
Grzegorz Krug <grzesiekk321@gmail.com>
2019-11-30 19:45:39 +0100

    redukcja

--- CyfrowePrzetwarzanieSygnalow/strategia_redukcji.py
+++ CyfrowePrzetwarzanieSygnalow/strategia_redukcji.py
@@ -5,32 +5,37 @@ class listArray:
 	def __init__(self, array):
 		if type(array) == int:
 			array = [[array]]
-		elif type(array[0]) != int:
+		elif type(array[0]) == int:
 			array = [array]
 		
 		self.array = Matrix(array)
 		self.size = (len(array), len(array[0]))
+		print(f"Initated: \n{self.__repr__}\nSize: {self.size}")
 
 	def __repr__(self):
 		#self.__name__
 		txt = '['
 		for i in range(self.size[0]):
 			if i == 0:
-				txt += '['
+				txt += f"{'[':>2}"
 			else:
-				txt += '\n['
+				txt += f'\n{"[":>3}'
 			for j in range(self.size[1]):
-				txt +=  f' {self.size[0]*i +j}'
-			if i == self.size[1]-1:
+				txt +=  f' {self.array[self.size[0]*i +j]}'				
+			if i == self.size[1] - 1:
 				txt += ']'
 			else:
 				txt +=  f'],'
 		txt +=  f']'
+		# return str(self.array)
 		return txt				
 
 
-	def __subs__(self):
-		pass
+	def __sub__(self, other):
+		return self.array - other.array
+
+	def __add__(self, other):
+		return self.array + other.array
 			
 # 			for col in row:
 # 				txt +=  f' {col}'
@@ -59,11 +64,14 @@ a = [['a','b', 'c'],
 
 A = listArray(a)
 print(A)
-#print(A[1,2])
-print(A*A-A)
+
+
+print(A - A)
+print(A + A)
 # for row in spA:
 	# print(row)
-
+print(Matrix(a)+Matrix(a))
+print(Matrix(a)-Matrix(a))
 # print(spA.det())
 #print(spA[3, 3])
 
