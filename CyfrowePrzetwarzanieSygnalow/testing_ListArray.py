a970afe7f4e751284ece32a0276d51fded8b7867
Grzegorz Krug <grzesiekk321@gmail.com>
2019-12-02 16:42:48 +0100

    transp

--- CyfrowePrzetwarzanieSygnalow/testing_ListArray.py
+++ CyfrowePrzetwarzanieSygnalow/testing_ListArray.py
@@ -27,15 +27,16 @@ class TestinglistArray(unittest.TestCase):
         assert A == Aprim
         assert not A == B
         assert cut_A == cut_B
-        
+
         # print("A2 \n\t", A[2])
         # print("List \n\t", ListArray(['q', 't', 'i', 'y']))
         assert A[2] == ListArray([['q', 't', 'i', 'y']])
         assert A[2] == ListArray(['q', 't', 'i', 'y'])
 
-    # def test_row_len(self):
-        # pass
-        # A = ListArray(self.a)
+    def test_row_len(self):        
+        A = ListArray(self.a)
+        for a in A:
+          print('a', a.transp())
 
     def test_fit(self):
         a = \
@@ -71,7 +72,7 @@ class TestinglistArray(unittest.TestCase):
         B = ListArray(b)
 
         A2 = ListArray(a2)
-        B2 = ListArray(b2)        
+        B2 = ListArray(b2)
         assert A.transp() == B
         assert A2.transp() == B2
 
@@ -86,12 +87,9 @@ class TestinglistArray(unittest.TestCase):
         B = ListArray(b)
         print(A*A)
 
-        for row in A:
-            print(row)
-        # for i in A:
-        #     print(i)
-        # for i in A.transp():
-        #     print(i)
+        # for row in A:
+        # print(row)
+
 
 if __name__ == '__main__':
     unittest.main()
