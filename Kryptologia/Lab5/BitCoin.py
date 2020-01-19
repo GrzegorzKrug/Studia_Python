f6948ec8876d328321d7883415ef945e0a693069
Grzegorz Krug <grzesiekk321@gmail.com>
2020-01-19 19:32:22 +0100

    BitCoin Validate

--- Kryptologia/Lab5/BitCoin.py
+++ Kryptologia/Lab5/BitCoin.py
@@ -19,10 +19,6 @@ class TimeServer:
 
 
 class BitCoinBlock:
-    shaHash = sha256()
-
-    def __init__(self):
-        pass
 
     def next_Block(self, pay_list, block, J):
         new_block = self.generate_Block(
@@ -44,14 +40,17 @@ class BitCoinBlock:
 
         timestamp = TimeServer.time_str()
         merkle_hash = self.merkle_tree(pay_list)
-        combine_text = timestamp + merkle_hash + str(num)
-        nonce, mine_time, final_hash = self.find_nonce(combine_text, J=J)
+        combine_text = previous_hash + str(num) + merkle_hash
+        nonce, mine_time, hashwithzeros = self.find_nonce(combine_text, J=J)
+
+        final_hash = self.hash(combine_text + str(nonce))
 
         block = {"block_num": num,
-                 "mine_time": mine_time,
                  "prev_hash": previous_hash,
-                 "merkle": merkle_hash,
+                 "merkle_hash": merkle_hash,
                  "nonce": nonce,
+                 "mine_time": round(mine_time, 6),
+                 "hashwithzeros": hashwithzeros,
                  "final_hash": final_hash}
         return block
 
@@ -59,21 +58,24 @@ class BitCoinBlock:
         flag_found = False
         time0 = time.time()
         nonce = 0
+
         while not flag_found:
-            final_hash = self.hash(str_input+str(nonce))
-            if final_hash.startswith("0"*J):
+            new_string = str_input+str(nonce)
+            hashwithzeros = self.hash(new_string)
+            if hashwithzeros.startswith("0"*J):
                 time_end = time.time()
                 flag_found = True
                 duration = time_end - time0
             else:
                 nonce += 1
 
-        return nonce, duration, final_hash
+        return nonce, duration, hashwithzeros
 
     @classmethod
     def hash(cls, text):
-        cls.shaHash.update(text.encode('utf-8'))
-        response = cls.shaHash.hexdigest()
+        shaHash = sha256()
+        shaHash.update(text.encode('utf-8'))
+        response = shaHash.hexdigest()
         return response
 
     def merkle_tree(self, transations_list):
@@ -103,35 +105,64 @@ class BitCoinBlock:
             if len(tree_leaves) == 1:
                 break
 
-        assert(len(tree_leaves) == 1), "Lenght of tree leaves is not 1"
-        # print("Leaves at end\n", tree_leaves)
+        assert(len(tree_leaves) == 1), "Lenght of tree leaves is not 1"        
         return tree_leaves[0]
 
 
-class Miner:
-    pass
+def validate(block_list):
+    for i in range(len(block_list)-1, 0, -1):
+        current_block = block_list[i]
+        prev_block = block_list[i-1]
+
+        text = current_block['prev_hash'] \
+            + str(current_block['block_num']) \
+            + current_block['merkle_hash'] \
+            + str(current_block['nonce'])
+
+        proof_of_work = BitCoinBlock.hash(text)
+
+        hash_proof = BitCoinBlock.hash(prev_block['prev_hash']
+                                       + str(prev_block['block_num'])
+                                       + prev_block['merkle_hash']
+                                       + str(prev_block['nonce']))
+
+        if not proof_of_work.startswith("0"*J) \
+                or current_block['prev_hash'] != hash_proof:
+            return False
+    return True
+
+
+def pretty_print(bc_block):
+    print("="*30)
+    for key, val in bc_block.items():
+        print(key.ljust(15), ":", val)
+    print("="*30, "\n")
 
 
 pay_list1 = ['Bob pays Alice',
              'Tom pays bob']
 pay_list2 = ["Trump buys wall",
              "Putin hires spies",
-             "Iran buys rockets"]
+             "Iran buys rockets",
+             "Elon invest in GIGA factory"]
 
 J = 5
-
 BC = BitCoinBlock()
 
-print("="*30, "\n")
+
 init_block = BC.generate_Block(J=J)
-print(init_block)
+pretty_print(init_block)
 
-print("="*30, "\n")
+# print("="*30, "\n")
 block1 = BC.next_Block(pay_list1, init_block, J=J)
-print(block1)
+pretty_print(block1)
 
-print("="*30, "\n")
+# print("="*30, "\n")
 block2 = BC.next_Block(pay_list2, block1, J=J)
-print(block2)
+pretty_print(block2)
 # for x in range(5):
 #     abc.find_hash(test_string, 5)
+
+
+val = validate([init_block, block1, block2])
+print("Validate:", val)
